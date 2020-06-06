bf89dac8952ecadb9281faff927ae149dae1c428
rui-watanabe <wataru610806@gmail.com>
2020-06-06 09:10:47 +0900

    react type usestate

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 type HelloProps = {
   message: string;
 }
 
 const Hello = (props: HelloProps) => {
-  return <h1>Hello {props.message}</h1>
+  const [name, setName] = useState<string | null>("Max");
+  return <h1>Hello {`I am ${name}`}</h1>
 }
 
 export default Hello;
